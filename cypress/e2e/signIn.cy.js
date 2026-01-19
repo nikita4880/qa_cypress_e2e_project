e076b17f7800d0e5cca07c16b04071a468b2e602
niita4880 <niknikitich07@gmail.com>
2026-01-19 14:33:57 +0200

    first_version

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,34 +1,35 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
-
 import SignInPageObject from '../support/pages/signIn.pageObject';
 import HomePageObject from '../support/pages/home.pageObject';
-
 const signInPage = new SignInPageObject();
 const homePage = new HomePageObject();
-
 describe('Sign In page', () => {
   let user;
-
   before(() => {
     cy.task('db:clear');
     cy.task('generateUser').then((generateUser) => {
       user = generateUser;
     });
   });
-
   it('should provide an ability to log in with existing credentials', () => {
     signInPage.visit();
     cy.register(user.email, user.username, user.password);
-
     signInPage.typeEmail(user.email);
     signInPage.typePassword(user.password);
     signInPage.clickSignInBtn();
-
     homePage.assertHeaderContainUsername(user.username);
   });
 
   it('should not provide an ability to log in with wrong credentials', () => {
+    signInPage.visit();
 
+    signInPage.typeEmail(user.email);
+    signInPage.typePassword(user.password + 'wrong111');
+    signInPage.clickSignInBtn();
+
+    cy.contains('div[class="swal-title"]', 'Login failed!').should(
+      'be.visible'
+    );
   });
 });
